809204aa6b68847dab3130a6636ade82dfadf6b7
chetan <chumblechetan@gmail.com>
2021-07-27 23:34:03 +0530

    test

--- src/app/Pages/promotions/promotions.service.ts
+++ src/app/Pages/promotions/promotions.service.ts
@@ -4,6 +4,7 @@ import { Observable, throwError } from 'rxjs';
 import { retry, catchError } from 'rxjs/operators';
 import { environment } from 'src/environments/environment';
 
+
 @Injectable({
   providedIn: 'root'
 })
